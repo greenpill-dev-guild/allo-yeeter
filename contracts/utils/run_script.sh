40abb0853227e9c4fbf42a4e3bffce466588a2c6
Bertrand Juglas <b@juglas.name>
2025-03-20 00:38:30 +0100

    fix: update run_script.sh to use DEPLOYER_SCRIPT and improve script execution

--- contracts/utils/run_script.sh
+++ contracts/utils/run_script.sh
@@ -4,20 +4,23 @@
 source .env
 
 # Read script
-echo Which script do you want to run?
-read script
+# echo Which script do you want to run?
+# read script
+# DEPLOYER_SCRIPT=$1
 
 # Read script arguments
-echo Enter script arguments, or press enter if none:
-read -ra args
+# echo Enter script arguments, or press enter if none:
+# read -ra args
 
 # Run the script
-echo Running Script: $script...
+echo Running Script: $DEPLOYER_SCRIPT...
 
 # Run the script with interactive inputs
-forge script $script \
-    --rpc-url $RPC_URL \
+forge script script/$DEPLOYER_SCRIPT \
+    --chain sepolia \
+    --rpc-url $SEPOLIA_RPC_URL \
     --broadcast \
+    --verify \
     -vvvv \
-    --private-key $DEPLOYER_KEY \
-    $args
+    --private-key $DEPLOYER_PRIVATE_KEY
+# $args
