13a38df3729b7508f2b245545845d503fb83cded
Bertrand Juglas <b@juglas.name>
2025-03-19 23:42:01 +0100

    import console for enhanced logging in YeeterStrategy script

--- contracts/script/YeeterStrategy.s.sol
+++ contracts/script/YeeterStrategy.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Script} from "forge-std/Script.sol";
+import {Script, console} from "forge-std/Script.sol";
 
 import {YeeterStrategy} from "../src/YeeterStrategy.sol";
 
