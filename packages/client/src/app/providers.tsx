7518fcd5d0752a14a539f771f88978a74aeea40b
Afo <contact@afolabi.info>
2025-04-12 20:44:52 -0700

    removed API route on provider not needed

--- packages/client/src/app/providers.tsx
+++ packages/client/src/app/providers.tsx
@@ -8,30 +8,8 @@ export function AlloKitProviders({
   children: React.ReactNode;
 }>) {
   return (
-    <ApiProvider
-      api={{
-        upload: async (data) => {
-          return fetch(`/api/ipfs`, { method: "POST", body: toFormData(data) })
-            .then((r) => r.json())
-            .then((r) => r.cid);
-        },
-      }}
-    >
+    <ApiProvider>
       <Web3Provider>{children}</Web3Provider>
     </ApiProvider>
   );
-}
-
-function toFormData(data: File | Record<string, unknown> | FormData) {
-  const formData = new FormData();
-
-  if (!(data instanceof File)) {
-    const blob = new Blob([JSON.stringify(data)], {
-      type: "application/json",
-    });
-    data = new File([blob], "metadata.json");
-  }
-
-  formData.append("file", data);
-  return formData;
 }
