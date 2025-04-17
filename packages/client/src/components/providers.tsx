6a8d195528474b0d41f45c6da05f52689bf1ef8f
Afo <contact@afolabi.info>
2025-04-17 00:28:23 -0700

    fixed regression with IPFS route

--- packages/client/src/components/providers.tsx
+++ packages/client/src/components/providers.tsx
@@ -8,8 +8,30 @@ export function AlloKitProviders({
   children: React.ReactNode;
 }>) {
   return (
-    <ApiProvider>
+    <ApiProvider
+      api={{
+        upload: async (data) => {
+          return fetch(`/api/ipfs`, { method: "POST", body: toFormData(data) })
+            .then((r) => r.json())
+            .then((r) => r.cid);
+        },
+      }}
+    >
       <Web3Provider>{children}</Web3Provider>
     </ApiProvider>
   );
+}
+
+function toFormData(data: File | Record<string, unknown> | FormData) {
+  const formData = new FormData();
+
+  if (!(data instanceof File)) {
+    const blob = new Blob([JSON.stringify(data)], {
+      type: "application/json",
+    });
+    data = new File([blob], "metadata.json");
+  }
+
+  formData.append("file", data);
+  return formData;
 }
